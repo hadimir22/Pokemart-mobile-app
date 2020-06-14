f29b595cc4db53d8b93563b908140b548d8694bd
hadimir22 <hadimir22@gmail.com>
2020-06-14 12:55:19 +0530

    feat: product list added in landing screen

--- src/screens/landing/index.js
+++ src/screens/landing/index.js
@@ -7,6 +7,8 @@ import {
   View,
   SafeAreaView,
   ScrollView,
+  TouchableOpacity,
+  FlatList,
 } from 'react-native';
 import {
   backgroundColorPrimary,
@@ -16,7 +18,113 @@ import {
 import FeatherIcon from 'react-native-vector-icons/dist/Feather';
 import ProductCarousel from '../../common/carousel';
 import {iconColorPrimary} from '../../constants/index';
-import {from} from 'rxjs';
+import StarRatingComponent from '../../common/starRating';
+
+const pokemon = [
+  {
+    id: 1,
+    name: 'Pikachu',
+    star: 5,
+    image: '../../assets/pokemons/pokeball.png',
+  },
+  {
+    id: 2,
+    name: 'One Plus',
+    star: 5,
+    image: '../../assets/pokemons/pokeball.png',
+  },
+  {
+    id: 3,
+    name: 'Hero Go Pro',
+    star: 2,
+    image: '../../assets/pokemons/pokeball.png',
+  },
+  {
+    id: 4,
+    name: 'Backpack',
+    image: '../../assets/pokemons/pokeball.png',
+  },
+  {
+    id: 5,
+    name: 'Backpack',
+    star: 1,
+    image: '../../assets/pokemons/pokeball.png',
+  },
+  {
+    id: 6,
+    name: 'Backpack',
+    star: 4,
+    image: '../../assets/pokemons/pokeball.png',
+  },
+  {
+    id: 7,
+    name: 'Mac Book',
+    star: 3,
+    image: '../../assets/pokemons/pokeball.png',
+  },
+  {
+    id: 8,
+    name: 'One Plus',
+    star: 5,
+    image: '../../assets/pokemons/pokeball.png',
+  },
+  {
+    id: 9,
+    name: 'Hero Go Pro',
+    star: 5,
+    image: '../../assets/pokemons/pokeball.png',
+  },
+  {
+    id: 10,
+    name: 'Backpack',
+    star: 5,
+    image: '../../assets/pokemons/pokeball.png',
+  },
+];
+
+class Product extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View
+        style={{
+          backgroundColor: 'grey',
+          margin: 2,
+          flex: 0.5,
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 20,
+        }}>
+        <Image
+          source={require('../../assets/pokemons/pokeball.png')}
+          style={{height: 50, width: 50}}
+        />
+        <Text>{this.props.pokemon.name}</Text>
+        <View style={{marginVertical: 3}}>
+          <StarRatingComponent
+            rating={this.props.pokemon.star}
+            disabled={true}
+          />
+        </View>
+
+        <View style={{flexDirection: 'row', marginTop: 8}}>
+          <TouchableOpacity
+            style={[styles.action, {borderColor: 'green'}]}
+            activeOpacity={0.7}>
+            <FeatherIcon name="shopping-cart" size={25} color="green" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.action, {borderColor: 'red'}]}
+            activeOpacity={0.7}>
+            <FeatherIcon name="heart" size={25} color="red" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
 
 export default class Home extends PureComponent {
   constructor(props) {
@@ -42,11 +150,17 @@ export default class Home extends PureComponent {
                 paddingVertical: 20,
                 flexDirection: 'row',
                 width: '90%',
+                borderRadius: 15,
                 position: 'absolute',
                 bottom: -15,
               }}>
               <View
-                style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                style={{
+                  flexDirection: 'row',
+                  flex: 1,
+                  justifyContent: 'space-between',
+                  paddingHorizontal: 10,
+                }}>
                 <FeatherIcon name="gift" size={45} color={iconColorPrimary} />
                 <FeatherIcon
                   name="activity"
@@ -62,6 +176,13 @@ export default class Home extends PureComponent {
               </View>
             </View>
           </View>
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            data={pokemon}
+            renderItem={({item}) => <Product pokemon={item} />}
+            keyExtractor={item => item.id}
+            numColumns={2}
+          />
         </ScrollView>
       </SafeAreaView>
     );
@@ -78,5 +199,11 @@ const styles = StyleSheet.create({
   text: {
     fontSize: width * 0.5,
     textAlign: 'center',
+  },
+  action: {
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 5,
+    marginLeft: 5,
   },
 });
