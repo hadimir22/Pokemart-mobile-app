f231bdb49b05fa5de2e6d70d3f1778b79595e28d
hadimir22 <hadimir22@gmail.com>
2020-06-17 19:59:36 +0530

    feat: splash screen added

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
 import React, {Component} from 'react';
+import {Image, View, Dimensions, Text, StatusBar} from 'react-native';
 import 'react-native-gesture-handler';
 import StackNav from './src';
+import Splash from './src/assets/images/splash.jpeg';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      showSplash: true,
+    };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({showSplash: false});
+    }, 2500);
+  }
 
   render() {
-    return <StackNav />;
+    return this.state.showSplash ? (
+      <View style={{flex: 1}}>
+        <StatusBar hidden={true} />
+        <Image
+          source={Splash}
+          style={{
+            alignSelf: 'center',
+            height: '100%',
+            width: '100%',
+          }}
+          resizeMode="stretch"
+        />
+      </View>
+    ) : (
+      <StackNav />
+    );
   }
 }
 
