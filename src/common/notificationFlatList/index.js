a1e43839894e0fe760dd43c652dbb392d59890a0
hadimir22 <hadimir22@gmail.com>
2020-06-26 07:37:13 +0530

    feat: orders displayed

--- src/common/notificationFlatList/index.js
+++ src/common/notificationFlatList/index.js
@@ -1,55 +1,28 @@
 import React, {Component} from 'react';
 import {SafeAreaView, View, FlatList, StyleSheet, Text} from 'react-native';
 import FeatherIcon from 'react-native-vector-icons/dist/Feather';
-import {colorWhite, iconColorPrimary, colorBlack} from '../../constants';
-
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    notification: 'Mac Book ',
-    notificationDetails: 'delivered',
-    time: '2 min ago',
-    icon: 'check-circle',
-    color: 'green',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    notification: 'One Plus',
-    notificationDetails: 'Cancelled',
-    time: '10 min ago',
-    icon: 'alert-triangle',
-    color: 'red',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    notification: 'Hero Go Pro  8',
-    notificationDetails: 'Shipped',
-    time: '40 min ago',
-    icon: 'gift',
-    color: 'purple',
-  },
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad535abb28ba',
-    notification: 'Backpack',
-    notificationDetails: 'On its way',
-    time: '50 min ago',
-    icon: 'compass',
-    color: 'brown',
-  },
-];
+import {
+  colorWhite,
+  iconColorPrimary,
+  colorBlack,
+  fontPoppinsBold,
+  fontPoppinsLight,
+} from '../../constants';
 
 function Item({item}) {
   return (
     <View style={styles.info}>
       <View>
-        <FeatherIcon name={item.icon} size={35} color={item.color} />
+        <FeatherIcon name="check-circle" size={35} color="green" />
       </View>
       <View style={{paddingHorizontal: 30}}>
-        <Text style={[styles.title, {fontWeight: 'bold'}]}>
-          {item.notification}
+        <Text style={[styles.title, {fontFamily: fontPoppinsBold}]}>
+          {item} Pokemon ordered
         </Text>
-        <Text style={styles.title}>{item.notificationDetails}</Text>
-        <Text style={styles.title}>{item.time}</Text>
+        <Text style={[styles.title, {fontFamily: fontPoppinsLight}]}>
+          {item > 1 ? 'Pokemons' : 'Pokemon'} on {item > 1 ? 'their' : 'its'}{' '}
+          way
+        </Text>
       </View>
     </View>
   );
@@ -58,17 +31,13 @@ function Item({item}) {
 class NotificationFlatListComponent extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      data: this.props.tabData,
-    };
   }
   render() {
     return (
       <View style={styles.container}>
         <FlatList
           showsVerticalScrollIndicator={false}
-          data={DATA}
+          data={this.props.orders}
           renderItem={({item}) => <Item item={item} />}
           keyExtractor={item => item.id}
         />
--- src/screens/notifications/index.js
+++ src/screens/notifications/index.js
@@ -1,29 +1,63 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, ActivityIndicator} from 'react-native';
 import {withNavigation} from 'react-navigation';
 import Empty from '../../common/emptyScreen';
 import NotificationFlatListComponent from '../../common/notificationFlatList';
+import AsyncStorage from '@react-native-community/async-storage';
 
 class Notifications extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      notifications: null,
+      orders: null,
+      loading: true,
     };
+
+    const {navigation} = this.props;
+    this.focusListener = navigation.addListener('didFocus', async () => {
+      // this.setState({loading: true, cartdata: null});
+      // this.getCartItems();
+      alert('focused');
+    });
   }
 
-  componentDidMount() {}
+  getData = async key => {
+    try {
+      const jsonValue = await AsyncStorage.getItem(key);
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      // error reading value
+      console.log(e);
+    }
+  };
+
+  getOrders = async () => {
+    let items = await this.getData('order');
+    console.log('cat items', items);
+
+    setTimeout(() => {
+      this.setState({orders: items, loading: false}, () => {
+        console.log('orders', this.state);
+      });
+    }, 500);
+  };
+
+  componentDidMount() {
+    this.getOrders();
+  }
 
   render() {
     return (
       <View style={{flex: 1}}>
-        {this.state.notifications ? (
+        {this.state.loading ? (
+          <View style={{marginVertical: '50%'}}>
+            <ActivityIndicator size="large" color="tomato" />
+          </View>
+        ) : this.state.orders && this.state.orders.length == 0 ? (
           <Empty icon="bell" text="No notifications" />
         ) : (
           <View style={{flex: 1}}>
-            <NotificationFlatListComponent
-              notifications={this.state.notifications}
-            />
+            <NotificationFlatListComponent orders={this.state.orders} />
           </View>
         )}
       </View>
